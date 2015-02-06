e7fd62c4024aaf938ec1e8c08e1c20d4dbf29dba
Peter <gushakape@student.gu.se>
2015-02-06 11:25:44 +0100

    Hej hej

---
+++ ClubMain.java
@@ -0,0 +1,12 @@
+package myprogram.hmi.console;
+
+import myprogram.hmi.cli.ClubHelper;
+import com.sandklef.edu.ConsoleMenu.*;
+
+public class ClubMain{
+	public static void main(String [] args){
+
+		Menu m= new Menu();
+		m.run();
+	}
+}
---
+++ Menu.java
@@ -0,0 +1,5 @@
+import java.util*. 
+
+public class menu{
+	public static void main(String [] args)
+}
