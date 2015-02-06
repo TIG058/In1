2241bc8ed38f747cf3b9eb2aed0b845511968aee
Peter <gushakape@student.gu.se>
2015-02-06 12:56:28 +0100

    Skit
    wwwww

--- ClubMain1.java
+++ ClubMain1.java
@@ -3,7 +3,7 @@ package myprogram.hmi.console;
 import myprogram.hmi.cli.ClubHelper;
 import com.sandklef.edu.ConsoleMenu.*;
 
-public class ClubMain{
+public class ClubMain1{
 	public static void main(String [] args){
 
 		Menu m= new Menu();
