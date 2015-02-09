75ecff72229fce506ac66369772e9c3c2e21d812
Klaudia95 <klaudia.wybraniec@hotmail.com>
2015-02-09 15:23:46 +0100

    Försök till metoder som sorterar members efter id
    Metoden ska sortera members efter id nummer och skriva ut dem. Vi ska
    koppla klasser till databasfilen som innehåller members.

--- edu/sysvp/program/Controller.java
+++ edu/sysvp/program/Controller.java
@@ -1,6 +1,26 @@
+import java.util.*;
+
 public class Controller{
+
+
 	
-	public static listMemberAlpha(Member){
-		
+	public static void main(String [] args){
+		System.out.println("Members:");
+		for (Member m: members) {
+	    System.out.println(m);
+
+	}	
 	}
+		//Menyalternativ 3, sortera på efternamn
+		
+	public static void printMembersId() {
+		Logger.debugM();
+		ArrayList<Member> members = cr.getMembers();
+		Collections.sort(members, new MemberIdComparator());
+
+	System.out.println("Members:");
+	for (Member m: members) {
+	    System.out.println(m);
+	}
+    }
 }
---
+++ edu/sysvp/program/ListTester.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ListTester{
+	
+	public static void main(String[]args){
+
+		System.out.println(printMembersId());
+
+	}
+}
